0b9f137e82c3638de3b142165fc89686189fed62
TheeNtsakoSibisi <38327575+TheeNtsakoSibisi@users.noreply.github.com>
2021-02-14 08:43:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const apiUrl = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
+const apiUrl = 'https://api.themoviedb.org/3/movie/550?api_key=97f722f59474aebe48831c3c90b71bc1';
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
-    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+    "https://api.themoviedb.org/3/search/movie?&api_key=97f722f59474aebe48831c3c90b71bc1&query=";
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
